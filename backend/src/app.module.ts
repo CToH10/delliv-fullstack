44e95482920ed0349ff6f587954a29b3255495ae
CToH10 <luisebn98@gmail.com>
2023-12-21 20:26:17 -0300

    feat: add base user dto

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AddressModule } from './address/address.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AddressModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/modules/users/dto/create-user.dto.ts
+++ backend/src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,44 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { hashSync } from 'bcryptjs';
+import { Transform } from 'class-transformer';
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(120)
+  username: string;
+
+  @ApiProperty()
+  @IsEmail()
+  @IsNotEmpty()
+  @MaxLength(120)
+  email: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(120)
+  fullName: string;
+
+  @ApiProperty()
+  @IsBoolean()
+  admin: boolean = false;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @Transform(({ value }: { value: string }) => hashSync(value), {
+    groups: ['transform'],
+  })
+  password: string;
+}
