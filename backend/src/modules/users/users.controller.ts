5669800e9de12266bb5547ecb559fc970f9b301c
CToH10 <luisebn98@gmail.com>
2023-12-23 15:17:34 -0300

    fix: user controller auth && docs

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JWTAuthGuard } from 'src/auth/jwt.auth.guard';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -12,23 +15,31 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
+  @ApiBearerAuth()
   @Get()
+  @UseGuards(JWTAuthGuard)
   findAll() {
     return this.usersService.findAll();
   }
 
+  @ApiBearerAuth()
   @Get(':id')
+  @UseGuards(JWTAuthGuard)
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
+  @ApiBearerAuth()
   @Patch(':id')
+  @UseGuards(JWTAuthGuard)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
+  @ApiBearerAuth()
   @HttpCode(204)
   @Delete(':id')
+  @UseGuards(JWTAuthGuard)
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
