58dd4583c279ce827d4d91d8105af5f4255f3896
CToH10 <luisebn98@gmail.com>
2023-12-23 10:50:56 -0300

    feat: add update user dto

--- backend/src/modules/users/dto/update-user.dto.ts
+++ backend/src/modules/users/dto/update-user.dto.ts
@@ -1,4 +1,37 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import {  ApiPropertyOptional } from '@nestjs/swagger';
+import { hashSync } from 'bcryptjs';
+import { Transform } from 'class-transformer';
+import { IsString, IsOptional, MaxLength, IsEmail, IsBoolean, MinLength } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    @MaxLength(120)
+    username: string;
+  
+    @ApiPropertyOptional()
+    @IsEmail()
+    @IsOptional()
+    @MaxLength(120)
+    email: string;
+  
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    @MaxLength(120)
+    fullName: string;
+  
+    @ApiPropertyOptional({ default: false })
+    @IsBoolean()
+    admin: boolean = false; 
+  
+    @ApiPropertyOptional()
+    @IsString()
+    @IsOptional()
+    @MinLength(8)
+    @Transform(({ value }: { value: string }) => hashSync(value), {
+      groups: ['transform'],
+    })
+    password: string;
+  }
