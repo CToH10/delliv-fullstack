c45526531aa7e955c127b4050a9b5e8e9a625175
CToH10 <luisebn98@gmail.com>
2024-01-09 13:46:45 -0300

    feat: add hover styles

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -8,16 +8,16 @@ export const Header = () => {
   return (
     <header className="flex flex-row justify-around w-full items-center h-16 border-b-2 border-brand-1 border-opacity-70  mb-24">
       {/* <Link to={<Home}>Delliv</Link> */}
-      <h1 className="text-brand-1 font-semibold text-heading4 w-2/5">Delliv</h1>
+      <h1 className="text-brand-1 font-semibold text-heading4 w-2/5 hover:font-bold hover:text-heading3 ">Delliv</h1>
       <nav className="w-1/4">
         <ul className="flex flex-row justify-between">
-          <li className="text-brand-2 font-medium text-heading5">
+          <li className="text-brand-2 font-medium text-heading5 hover:text-brand-3 hover:-translate-y-1 transition">
             <IoFastFoodSharp />
           </li>
-          <li className="text-brand-2 font-medium text-heading5">
+          <li className="text-brand-2 font-medium text-heading5 hover:text-brand-3 hover:-translate-y-1 transition">
             <BsBagHeartFill />
           </li>
-          <li className="text-brand-2 font-medium text-heading5">
+          <li className="text-brand-2 font-medium text-heading5 hover:text-brand-3 hover:-translate-y-1 transition">
             <IoPerson />
           </li>
         </ul>
