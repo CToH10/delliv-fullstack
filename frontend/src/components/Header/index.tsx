d800b14eb639aa79f8da88071ef757dae476aff7
CToH10 <luisebn98@gmail.com>
2024-01-10 17:18:22 -0300

    fix: link to home

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { BsBagHeartFill } from "react-icons/bs";
 import { IoFastFoodSharp } from "react-icons/io5";
 import { IoPerson } from "react-icons/io5";
@@ -7,10 +7,12 @@ import { IoPerson } from "react-icons/io5";
 export const Header = () => {
   return (
     <header className="flex flex-row justify-around w-full items-center h-16 border-b-2 border-brand-1 border-opacity-70  mb-24">
-      {/* <Link to={<Home}>Delliv</Link> */}
-      <h1 className="text-brand-1 font-semibold text-heading4 w-2/5 hover:font-bold hover:text-heading3">
+      <Link
+        to={"/"}
+        className="text-brand-1 font-semibold text-heading4 w-2/5 hover:font-bold hover:text-heading3"
+      >
         Delliv
-      </h1>
+      </Link>
       <nav className="w-1/4">
         <ul className="flex flex-row justify-between">
           <li className="text-brand-2 font-medium text-heading5 hover:text-brand-3 hover:-translate-y-1 transition">
