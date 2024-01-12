5c67153759bb8465b115d00fbaeea53ce296e73a
CToH10 <luisebn98@gmail.com>
2024-01-12 16:43:31 -0300

    feat: add login and register redirection

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -33,7 +33,7 @@ export const Header = () => {
                 <IoPerson />
               </Link>
             ) : (
-              <Link to={"/register"}>
+              <Link to={"/login"}>
                 <IoPersonAdd />
               </Link>
             )}
--- frontend/src/pages/loginPage.tsx
+++ frontend/src/pages/loginPage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Layout } from "./layout";
 import { LoginForm } from "../components/Forms/User/LoginForm";
+import { LoginOrRegisterBox } from "../components/LoginOrRegister";
 
 export const LoginPage = () => {
   return (
     <Layout>
-      <div className="w-4/5 h-[160px] m-auto flex flex-col items-center justify-center mt-[50px] scrollbar">
+      <LoginOrRegisterBox isLogin />
+      <div className="w-4/5 m-auto flex flex-col items-center justify-center mt-[35px] scrollbar">
         <LoginForm />
       </div>
     </Layout>
