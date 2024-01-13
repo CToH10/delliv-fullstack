b5689f06d9b10fcabcc0c2cead9180c4106188e4
CToH10 <luisebn98@gmail.com>
2024-01-13 16:07:56 -0300

    feat: add logout button -nofuction

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { BsCartCheckFill, BsCart } from "react-icons/bs";
-import { IoFastFoodSharp, IoPersonAdd, IoPerson } from "react-icons/io5";
+import {
+  IoFastFoodSharp,
+  IoPersonAdd,
+  IoPerson,
+  IoLogOut,
+} from "react-icons/io5";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
 import { cartFullQuantity } from "../../utils/cartFullQuantity";
@@ -11,41 +16,48 @@ export const Header = () => {
   const cart = useSelector((state: RootState) => state.cart.cart);
 
   return (
-    <header className="flex flex-row justify-around w-full items-center h-16 border-b-2 border-brand-1 border-opacity-70 mb-4">
-      <Link
-        to={"/"}
-        className="text-brand-1 font-semibold text-heading4 w-2/5 hover:font-bold hover:text-heading3"
-      >
-        Delliv
-      </Link>
-      <nav className="w-1/4">
-        <ul className="flex flex-row justify-between">
-          <li className="text-brand-2 font-medium text-heading5 hover:text-brand-3 hover:-translate-y-1 transition">
-            <Link to={"/"}>
-              <IoFastFoodSharp />
-            </Link>
-          </li>
-          <li className="text-brand-2 font-medium text-heading5 hover:text-brand-3 hover:-translate-y-1 transition relative">
-            {cart.length > 0 ? <BsCartCheckFill /> : <BsCart />}
-            {cart.length > 0 && (
-              <span className="absolute -top-3 left-3 rounded-full bg-brand-4 z-10 text-heading8 px-2 py-1 font-bold">
-                {cartFullQuantity(cart)}
-              </span>
-            )}
-          </li>
-          <li className="text-brand-2 font-medium text-heading5 hover:text-brand-3 hover:-translate-y-1 transition">
-            {userToken ? (
-              <Link to={"/profile"}>
-                <IoPerson />
+    <header className="flex items-center justify-center w-full h-16 border-b-2 border-brand-1 border-opacity-70 mb-4">
+      <div className="flex flex-row justify-between items-center w-4/5 max-w-[1100px]">
+        <Link
+          to={"/"}
+          className="text-brand-1 font-semibold text-heading4 w-fit hover:font-bold hover:text-heading3"
+        >
+          Delliv
+        </Link>
+        <nav className="w-2/6 max-w-[300px]">
+          <ul className="flex flex-row justify-between min-w-full">
+            <li className="text-brand-2 font-medium text-heading5 hover:text-brand-3 hover:-translate-y-1 transition">
+              <Link to={"/"}>
+                <IoFastFoodSharp />
               </Link>
-            ) : (
-              <Link to={"/login"}>
-                <IoPersonAdd />
-              </Link>
+            </li>
+            <li className="text-brand-2 font-medium text-heading5 hover:text-brand-3 hover:-translate-y-1 transition relative">
+              {cart.length > 0 ? <BsCartCheckFill /> : <BsCart />}
+              {cart.length > 0 && (
+                <span className="absolute -top-3 left-3 rounded-full bg-brand-4 z-10 text-heading8 px-2 py-1 font-bold">
+                  {cartFullQuantity(cart)}
+                </span>
+              )}
+            </li>
+            <li className="text-brand-2 font-medium text-heading5 hover:text-brand-3 hover:-translate-y-1 transition">
+              {userToken ? (
+                <Link to={"/profile"}>
+                  <IoPerson />
+                </Link>
+              ) : (
+                <Link to={"/login"}>
+                  <IoPersonAdd />
+                </Link>
+              )}
+            </li>
+            {userToken && (
+              <li className="text-brand-2 font-medium text-heading5 hover:text-brand-3 hover:-translate-y-1 transition">
+                <IoLogOut />
+              </li>
             )}
-          </li>
-        </ul>
-      </nav>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
