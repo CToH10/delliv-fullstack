28ffb8d45a7bc5e78058db5487ad000b923d2158
CToH10 <luisebn98@gmail.com>
2024-01-13 11:47:32 -0300

    feat: add user info

--- frontend/src/components/Profile/ProfileCard/index.tsx
+++ frontend/src/components/Profile/ProfileCard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useUserCont, userProfile } from "../../../context/userContext";
+import { IoPencilSharp } from "react-icons/io5";
 
 export const ProfileCard = () => {
   const { getUserInfo } = useUserCont();
@@ -16,28 +17,42 @@ export const ProfileCard = () => {
   }, []);
 
   return (
-    <div>
-      <h2>{userInfo ? userInfo.fullName : "Carregando..."}</h2>
-      <h2>{userInfo ? userInfo.username : "Carregando..."}</h2>
-      <h2>{userInfo ? userInfo.email : "Carregando..."}</h2>
-      <section className="address">
-        <div className="flex flex-row">
-          <h3>Endereço</h3>
-          <button type="button">Pen</button>
+    <div className="infoCard">
+      <section className="userInfo flex flex-col gap-2">
+        <h2 className="textOverflow">
+          {userInfo ? userInfo.fullName : "Carregando..."}
+        </h2>
+        <h2 className="textOverflow">
+          {userInfo ? userInfo.username : "Carregando..."}
+        </h2>
+        <h2 className="textOverflow">
+          {userInfo ? userInfo.email : "Carregando..."}
+        </h2>
+      </section>
+      <section className="address flex flex-col gap-2">
+        <div className="flex flex-row justify-between">
+          <h3 className="text-brand-1 font-extrabold">Endereço</h3>
+          <button type="button" onClick={() => console.log("edit")} className="btn-brand1 btn-small">
+            {<IoPencilSharp />}
+          </button>
         </div>
         <div>
-          <h3>
+          <h3 className="textOverflow">
             {userInfo
               ? `${userInfo.address.street}, ${userInfo.address.number}`
               : "Carregando"}
           </h3>
-          <h3>{userInfo?.address.complement && userInfo.address.complement}</h3>
-          <h3>
+          <h3 className="textOverflow">
+            {userInfo?.address.complement && userInfo.address.complement}
+          </h3>
+          <h3 className="textOverflow">
             {userInfo
               ? `${userInfo.address.city} - ${userInfo.address.state}`
               : "Carregando"}
           </h3>
-          <h3>{userInfo ? userInfo.address.cep : "Carregando"}</h3>
+          <h3 className="textOverflow">
+            {userInfo ? userInfo.address.cep : "Carregando"}
+          </h3>
         </div>
       </section>
     </div>
